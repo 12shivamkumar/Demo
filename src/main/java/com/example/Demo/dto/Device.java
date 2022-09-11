fac9c269699d11ceb94e2d266ee7867185ca5331
shivamGupta <shivamguptaniki@gmail.com>
2022-09-11 17:05:03 +0530

    added device class

---
+++ src/main/java/com/example/Demo/dto/Device.java
@@ -0,0 +1,29 @@
+package com.example.Demo.dto;
+
+public class Device {
+
+    private String deviceName;
+
+    private int devicePrice;
+
+    public Device() {
+    }
+
+    public String getDeviceName() {
+        return deviceName;
+    }
+
+    public void setDeviceName(String deviceName) {
+        this.deviceName = deviceName;
+    }
+
+    public int getDevicePrice() {
+        return devicePrice;
+    }
+
+    public void setDevicePrice(int devicePrice) {
+        this.devicePrice = devicePrice;
+    }
+}
+
+
