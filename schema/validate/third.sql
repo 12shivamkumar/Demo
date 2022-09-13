95f8959db68ef35750f757205087ed0dad7213fe
shivamGupta <shivamguptaniki@gmail.com>
2022-09-13 15:56:22 +0530

    changes

--- schema/validate/third.sql
+++ schema/validate/third.sql
@@ -1,7 +1,7 @@
 CREATE TABLE employee (
 id varchar(10),
 name varchar(20) NOT NULL,
-office_id tinyint,
+email varchar(30) NOT NULL UNIQUE,
 created_date timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
 auto_update_time timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 PRIMARY KEY (id),
