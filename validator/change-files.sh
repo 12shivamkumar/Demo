8e98999da2722aa7afdf757479f2ddae93f21341
shivamGupta <shivamguptaniki@gmail.com>
2022-09-13 18:18:56 +0530

    changes

--- validator/change-files.sh
+++ validator/change-files.sh
@@ -2,15 +2,20 @@
 
 declare -a files_list
 files_list=$1
+
+echo ${files_list}
+
 declare -A schema_list
 declare -A seed_data_list
 for i in ${files_list[*]}
 do
 if [[ "$i" =~ ^schema.* ]]; then
   key=$(echo $i | cut -d'/' -f 1-)
+  echo $key
   schema_list["${key}"]=$i
 elif [[ "$i" =~ ^seed_data.*  ]]; then
   key=$(echo $i | cut -d'/' -f 1-)
+  echo $key
   seed_data_list["${key}"]=$i
   if [[ ! -v schema_list["${key}"] ]]; then
     schema_list["${key}"]="schema/""$key"
@@ -26,3 +31,5 @@ fi
 echo "::set-output name=schema_files::" "${schema_list[@]}"
 echo "::set-output name=seed_data_files::" "${seed_data_list[@]}"
 echo "::set-output name=db_file_change_exists::$change_in_db_files"
+
+echo ${schema_files}
