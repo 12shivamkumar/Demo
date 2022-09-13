7f677a535e500bab068e04bdd4d4a44c81b044dd
shivamGupta <shivamguptaniki@gmail.com>
2022-09-13 18:33:56 +0530

    changes

--- schema/validate/eight.sql
+++ schema/validate/eight.sql
@@ -1,10 +1,6 @@
-CREATE TABLE meeting_room(
-id int AUTO_INCREMENT,
-room_name varchar(15) NOT NULL UNIQUE,
-office_id tinyint,
-is_open boolean NOT NULL DEFAULT TRUE,
-created_date timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-updated_date timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-PRIMARY KEY(id),
-INDEX(office_id, room_name)
+CREATE TABLE employee_meeting(
+  employee_id varchar(10) ,
+  meeting_id int,
+  PRIMARY KEY ( employee_id , meeting_id),
+  INDEX(date, status)
 );
