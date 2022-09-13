b4ebfc21be547452802e2d2106cda87343e32f5f
shivamGupta <shivamguptaniki@gmail.com>
2022-09-13 15:26:09 +0530

    changes

--- validator/mysql-execution.sh
+++ validator/mysql-execution.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 ret=0
-sudo sudo apt-get install libmysql-diff-perl
+sudo apt-get install libmysql-diff-perl
 echo $1
 
 echo $2
@@ -8,7 +8,6 @@ echo $2
 for i in $1
    do
    echo "$i"
-   mysql-schema-diff schema/validate/one.sql $1
    done
 
 exit $ret
