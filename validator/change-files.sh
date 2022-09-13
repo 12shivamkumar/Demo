872cdb526487a665c384c117ac2f366c19582c7c
shivamGupta <shivamguptaniki@gmail.com>
2022-09-13 19:34:45 +0530

    changes

--- validator/change-files.sh
+++ validator/change-files.sh
@@ -6,20 +6,11 @@ files_list=$1
 echo ${files_list}
 
 declare -A schema_list
-declare -A seed_data_list
 for i in ${files_list[*]}
 do
 if [[ "$i" =~ ^schema.* ]]; then
   key=$(echo $i | cut -d'/' -f 2-)
-  echo schema $key
   schema_list["${key}"]=$i
-elif [[ "$i" =~ ^seed_data.*  ]]; then
-  key=$(echo $i | cut -d'/' -f 2-)
-  echo seed $key
-  seed_data_list["${key}"]=$i
-  if [[ ! -v schema_list["${key}"] ]]; then
-    schema_list["${key}"]="schema/""$key"
-  fi
 fi
 done
 
@@ -27,7 +18,7 @@ echo ${schema_list[@]}
 
 change_in_db_files=false
 echo $change_in_db_files
-if [[ ${#schema_list[@]} -gt 0 || ${#seed_data_list[@]} -gt 0 ]]; then
+if [[ ${#schema_list[@]} -gt 0 ]]; then
   change_in_db_files=true
 fi
 
@@ -35,7 +26,6 @@ echo ${schema_list[@]}
 echo $change_in_db_files
 
 echo "::set-output name=schema_files::" "${schema_list[@]}"
-echo "::set-output name=seed_data_files::" "${seed_data_list[@]}"
 echo "::set-output name=db_file_change_exists::$change_in_db_files"
 
 echo ${schema_files}
