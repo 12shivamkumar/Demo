2154e324c8416170183b09869c1a5bee08caf6d5
shivamGupta <shivamguptaniki@gmail.com>
2022-09-13 16:29:45 +0530

    change

--- validator/mysql-execution.sh
+++ validator/mysql-execution.sh
@@ -4,7 +4,7 @@ sudo apt-get install libmysql-diff-perl
 
 sudo /etc/init.d/mysql start
 mysql -e 'SET GLOBAL sql_mode = NO_ZERO_DATE;' -uroot -proot
-mysql -e 'CREATE USER 'runner'@'localhost' IDENTIFIED BY '';' -uroot -proot
+mysql -e 'CREATE USER 'runner'@'localhost' IDENTIFIED BY '123';' -uroot -proot
 
 echo $1
 
