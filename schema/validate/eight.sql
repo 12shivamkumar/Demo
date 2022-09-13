06c407b2c93125c940b7ecd42688f44a5554a0a2
shivamGupta <shivamguptaniki@gmail.com>
2022-09-13 18:37:31 +0530

    changes

--- schema/validate/eight.sql
+++ schema/validate/eight.sql
@@ -1,6 +1,14 @@
-CREATE TABLE employee_meeting(
-  employee_id varchar(10) ,
-  meeting_id int,
-  PRIMARY KEY ( employee_id , meeting_id),
-  INDEX(date, status)
+CREATE TABLE meeting(
+meet_id int AUTO_INCREMENT,
+description varchar(200),
+agenda varchar(100) NOT NULL,
+owner_id varchar(10) NOT NULL,
+date_of_meeting date NOT NULL,
+start_time time NOT  NULL,
+end_time time NOT NULL,
+is_available boolean NOT NULL DEFAULT TRUE,
+room_id smallint ,
+created_date timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+PRIMARY KEY (meet_id),
+INDEX (room_id, date_of_meeting, is_available)
 );
