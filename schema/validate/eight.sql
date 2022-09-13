1d4c735ce5992b8510a67a8dea1b423823380fd0
shivamGupta <shivamguptaniki@gmail.com>
2022-09-13 18:25:00 +0530

    changes

--- schema/validate/eight.sql
+++ schema/validate/eight.sql
@@ -1,14 +1,10 @@
-CREATE TABLE meeting(
-meet_id int AUTO_INCREMENT,
-description varchar(200),
-agenda varchar(100) NOT NULL,
-owner_id varchar(10) NOT NULL,
-date_of_meeting date NOT NULL,
-start_time time NOT  NULL,
-end_time time NOT NULL,
-is_available boolean NOT NULL DEFAULT TRUE,
-room_id smallint ,
+CREATE TABLE meeting_room(
+id int AUTO_INCREMENT,
+room_name varchar(15) NOT NULL UNIQUE,
+office_id tinyint,
+is_open boolean NOT NULL DEFAULT TRUE,
 created_date timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-PRIMARY KEY (meet_id),
-INDEX (room_id, date_of_meeting, is_available)
+updated_date timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+PRIMARY KEY(id),
+INDEX(office_id, room_name)
 );
--- validator/change-files.sh
+++ validator/change-files.sh
@@ -10,12 +10,12 @@ declare -A seed_data_list
 for i in ${files_list[*]}
 do
 if [[ "$i" =~ ^schema.* ]]; then
-  key=$(echo $i | cut -d'/' -f 1-)
-  echo $key
+  key=$(echo $i | cut -d'/' -f 2-)
+  echo schema $key
   schema_list["${key}"]=$i
 elif [[ "$i" =~ ^seed_data.*  ]]; then
-  key=$(echo $i | cut -d'/' -f 1-)
-  echo $key
+  key=$(echo $i | cut -d'/' -f 2-)
+  echo seed $key
   seed_data_list["${key}"]=$i
   if [[ ! -v schema_list["${key}"] ]]; then
     schema_list["${key}"]="schema/""$key"
@@ -23,10 +23,16 @@ elif [[ "$i" =~ ^seed_data.*  ]]; then
 fi
 done
 
+echo ${schema_list}
+echo $change_in_db_files
+
 change_in_db_files=false
 if [[ ${#schema_list[@]} -gt 0 || ${#seed_data_list[@]} -gt 0 ]]; then
   change_in_db_files=true
 fi
+
+echo ${schema_list}
+echo $change_in_db_files
 
 echo "::set-output name=schema_files::" "${schema_list[@]}"
 echo "::set-output name=seed_data_files::" "${seed_data_list[@]}"
