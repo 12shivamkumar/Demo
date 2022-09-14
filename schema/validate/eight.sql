fb1155f4fa77c77274b492ef3e2ce4bf833ddb61
shivamGupta <shivamguptaniki@gmail.com>
2022-09-14 10:51:49 +0530

    changes

--- schema/validate/eight.sql
+++ schema/validate/eight.sql
@@ -1,6 +1,5 @@
 CREATE TABLE employee_meeting(
   employee_id varchar(10) ,
   meeting_id int,
-  PRIMARY KEY ( employee_id , meeting_id),
-  INDEX(date, status)
+  PRIMARY KEY ( employee_id , meeting_id)
 );
--- schema/validate/one.sql
+++ schema/validate/one.sql
@@ -4,5 +4,5 @@ name varchar(20) NOT NULL,
 email varchar(30) NOT NULL UNIQUE,
 created_date timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
 auto_update_time timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-PRIMARY KEY (id),
-INDEX(office_id));
+PRIMARY KEY (id)
+);
