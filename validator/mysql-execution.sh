3b33d1bb701bbfacb11f4fa3bed2e10a11e88689
shivamGupta <shivamguptaniki@gmail.com>
2022-09-14 16:31:11 +0530

    change mysql-execution.sh

--- validator/mysql-execution.sh
+++ validator/mysql-execution.sh
@@ -2,12 +2,8 @@
 sudo apt-get install libmysql-diff-perl
 for i in $1
    do
-   echo "$i"
-   echo master_branch
    touch temp.sql
    git show b56201157c488331d24cae81350a91869a06a24d:$i > temp.sql
    cat temp.sql
-   echo test_branch
-   cat $i
    echo
    done
