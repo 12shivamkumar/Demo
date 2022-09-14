ef1d64be30559d368b7000955bc3fc11df3dd6bc
shivamGupta <shivamguptaniki@gmail.com>
2022-09-14 16:15:08 +0530

    change mysql-execution.sh

--- validator/mysql-execution.sh
+++ validator/mysql-execution.sh
@@ -6,5 +6,6 @@ for i in $1
    git show b56201157c488331d24cae81350a91869a06a24d:$i
    echo test_branch
    cat $i
+   mysql-schema-diff  | git show b56201157c488331d24cae81350a91869a06a24d:$i $i | grep -c "DROP"
    echo
    done
