fb033a18d4fa411f2c53dd979a8dd06e819ba574
shivamGupta <shivamguptaniki@gmail.com>
2022-09-14 16:25:59 +0530

    change mysql-execution.sh

--- validator/mysql-execution.sh
+++ validator/mysql-execution.sh
@@ -4,7 +4,8 @@ for i in $1
    do
    echo "$i"
    echo master_branch
-   git show b56201157c488331d24cae81350a91869a06a24d:$i | sudo nano temp.sql
+   touch temp.sql
+   git show b56201157c488331d24cae81350a91869a06a24d:$i > temp.sql
    cat $temp.sql
    cat temp.sql
    echo test_branch
