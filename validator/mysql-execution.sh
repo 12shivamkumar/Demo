2817ce616e5be5162867b870256dbd70d3a314a9
shivamGupta <shivamguptaniki@gmail.com>
2022-09-14 21:30:46 +0530

    checking whether user is added or not

--- validator/mysql-execution.sh
+++ validator/mysql-execution.sh
@@ -7,10 +7,10 @@ echo
 sudo systemctl status mysql
 echo
 
-sudo mysql -u root -p root
-mysql -e 'CREATE USER 'runner'@'localhost' IDENTIFIED BY 'runner123';' -u root -p root
-mysql -e 'GRANT ALL PRIVILEGES ON *.* TO 'runner'@'localhost' WITH GRANT OPTION;' -u root -p root
-mysql -e 'FLUSH PRIVILEGES;' -u root -p root
+sudo mysql -u root -p 'Shivam@123'
+mysql -e 'CREATE USER 'runner'@'localhost' IDENTIFIED BY 'runner123';' -u root -p 'Shivam@123'
+mysql -e 'GRANT ALL PRIVILEGES ON *.* TO 'runner'@'localhost' WITH GRANT OPTION;' -u root -p 'Shivam@123'
+mysql -e 'FLUSH PRIVILEGES;' -u root -p 'Shivam@123'
 
 declare -a value
 value=$2
@@ -29,6 +29,6 @@ for i in $1
    echo TEST_SQL_FILE
    cat $i
    echo
-   mysql-schema-diff temp.sql $i -u root -p root | grep -c "DROP"
+   mysql-schema-diff temp.sql $i -u root -p 'Shivam@123' | grep -c "DROP"
    done
 exit $ret
