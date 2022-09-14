5ee2222df4c8e45d9b6c14a55379ab0e3cd05046
shivamGupta <shivamguptaniki@gmail.com>
2022-09-14 16:22:38 +0530

    change mysql-execution.sh

--- validator/mysql-execution.sh
+++ validator/mysql-execution.sh
@@ -4,7 +4,9 @@ for i in $1
    do
    echo "$i"
    echo master_branch
-   git show b56201157c488331d24cae81350a91869a06a24d:$i
+   git show b56201157c488331d24cae81350a91869a06a24d:$i | sudo nano temp.sql
+   cat $temp.sql
+   cat temp.sql
    echo test_branch
    cat $i
    mysql-schema-diff $i $i | grep -c "DROP"
