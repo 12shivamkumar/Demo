f3b676e78fc3d82b7c21ac6de9484609b430bacd
shivamGupta <shivamguptaniki@gmail.com>
2022-09-14 10:46:44 +0530

    changes

--- validator/mysql-execution.sh
+++ validator/mysql-execution.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
 ret=0
-echo $1
-
-echo $2
-
+sudo /etc/init.d/mysql start
+mysql -e 'SET GLOBAL sql_mode = NO_ZERO_DATE;' -uroot -proot
+mysql -e 'CREATE DATABASE test;' -uroot -proot
+mysql -e 'SHOW DATABASES;' -uroot -proot
+mysql -e 'use test;' -uroot -proot
 for i in $1
    do
    echo "$i"
+   mysql -uroot -proot test < $i || ret=1
+   done
+for i in $2
+   do
+   echo "$i"
+   mysql -uroot -proot test < $i || ret=1
    done
 
 exit $ret
