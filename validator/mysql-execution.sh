2a7a0ce1f2fcf8250faa373b5fabfc925650aa65
shivamGupta <shivamguptaniki@gmail.com>
2022-09-14 16:10:10 +0530

    change mysql-execution.sh

--- validator/mysql-execution.sh
+++ validator/mysql-execution.sh
@@ -3,7 +3,7 @@ for i in $1
    do
    echo "$i"
    echo master_branch
-   git show ${github.event.pull_request.base.sha}:$i
+   git show ${{ github.event.pull_request.base.sha }}:$i
    echo test_branch
    cat $i
    echo
