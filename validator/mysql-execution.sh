fab35aa2fc55384f54e639650e23d3109c2f8ea3
shivamGupta <shivamguptaniki@gmail.com>
2022-09-14 16:33:23 +0530

    change mysql-execution.sh

--- validator/mysql-execution.sh
+++ validator/mysql-execution.sh
@@ -4,6 +4,8 @@ for i in $1
    do
    touch temp.sql
    git show b56201157c488331d24cae81350a91869a06a24d:$i > temp.sql
+   echo MASTER_SQL_FILE
    cat temp.sql
-   echo
+   echo TEST_SQL_FILE
+   echo $i
    done
