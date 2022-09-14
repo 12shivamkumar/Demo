e637718b6e9a9ffa2afd61c89be6717a2e5ce107
shivamGupta <shivamguptaniki@gmail.com>
2022-09-14 11:39:10 +0530

    changes

--- validator/mysql-execution.sh
+++ validator/mysql-execution.sh
@@ -1,15 +1,9 @@
 #!/bin/sh
 ret=0
-sudo /etc/init.d/mysql start
-mysql -e 'SET GLOBAL sql_mode = NO_ZERO_DATE;' -uroot -proot
-mysql -e 'CREATE DATABASE demo;' -uroot -proot
-mysql -e 'SHOW DATABASES;' -uroot -proot
-mysql -e 'use demo;' -uroot -proot
 for i in $1
    do
    echo "$i"
    cat $i
-   mysql -uroot -proot demo < $i || ret=1
    done
 
 exit $ret
