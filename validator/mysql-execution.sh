5ed219d8a27df55783e5c0041b86d618a815f30f
shivamGupta <shivamguptaniki@gmail.com>
2022-09-14 11:38:32 +0530

    changes

--- validator/mysql-execution.sh
+++ validator/mysql-execution.sh
@@ -11,17 +11,5 @@ for i in $1
    cat $i
    mysql -uroot -proot demo < $i || ret=1
    done
-for i in $2
-   do
-   echo "$i"
-   mysql -uroot -proot demo < $i || ret=1
-   done
-
-
-mysql -e 'DESCRIBE demo.employee;' -uroot -proot demo
-mysql -e 'DESCRIBE demo.office;' -uroot -proot demo
-mysql -e 'DESCRIBE demo.employees;' -uroot -proot demo
-mysql -e 'DESCRIBE demo.employee_meeting;' -uroot -proot demo
-
 
 exit $ret
