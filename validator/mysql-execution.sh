92a022f6d5789b9a69776a7ac50e2ee5f2f0599f
shivamGupta <shivamguptaniki@gmail.com>
2022-09-14 18:42:39 +0530

    added new branch develop

--- validator/mysql-execution.sh
+++ validator/mysql-execution.sh
@@ -1,7 +1,22 @@
 #!/bin/sh
+ret=0
+declare -a value
+value=$2
+
+echo $value
+
+
 for i in $1
    do
-   echo "$i"
+   touch temp.sql
+   echo $value~
+   git show $value~:$i > temp.sql
+   echo MASTER_SQL_FILE
+   cat temp.sql
+   echo
+   echo TEST_SQL_FILE
    cat $i
    echo
+   mysql-schema-diff temp.sql $i | grep -c "DROP"
    done
+exit $ret
