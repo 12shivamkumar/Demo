291fae1b9c89efdf70ad21d6db17335ebfe11985
shivamGupta <shivamguptaniki@gmail.com>
2022-09-15 12:02:28 +0530

    changed sql files for testing

--- schema/validate/four.sql
+++ schema/validate/four.sql
@@ -5,7 +5,6 @@ agenda varchar(100) NOT NULL,
 owner_id varchar(10) NOT NULL,
 is_available boolean NOT NULL DEFAULT TRUE,
 room_id smallint ,
-created_date timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
 PRIMARY KEY (meet_id),
 INDEX (room_id, is_available)
 );
